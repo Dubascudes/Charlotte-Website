56160fed483c12d92b77d7ff60e4910940f1700e
Will English <wenglish0223@gmail.com>
2023-05-05 20:47:53 -0400

    Add files via upload

--- main.php
+++ main.php
@@ -1,14 +1,4 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['inputFile'])) {
-    $target_dir = "/";
-    $target_file = $target_dir . basename($_FILES["inputFile"]["name"]);
-
-    if (move_uploaded_file($_FILES["inputFile"]["tmp_name"], $target_file)) {
-        $command = "bash /backend.sh " . escapeshellarg($target_file) . " 2>&1";
-        $output = shell_exec($command);
-        echo "<pre>$output</pre>";
-    } else {
-        echo "Error uploading file.";
-    }
-}
+$targetPath = "uploads/" . basename($_FILES["inputFile"]["name"]);
+move_uploaded_file($_FILES["inputFile"]["tmp_name"], $targetPath);
 ?>
